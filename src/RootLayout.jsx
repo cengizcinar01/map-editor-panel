f99b8d40f05f839edbc51fc44d3a85ceaa94d97a
Cengiz Cinar <c.cinar@mail.de>
2024-06-20 01:16:32 +0200

    Added Navbar to RootLayout and created new Navbar component with styling

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import SignIn from "./components/Auth/SignIn";
 import SignOut from "./components/Auth/SignOut";
+import Navbar from "./components/Navbar/Navbar";
 
 const RootLayout = () => {
   return (
     <>
+      <Navbar />
       <Outlet />
       <SignIn />
       <SignOut />
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className={styles["navbar-container"]}></div>
+    </>
+  );
+};
+
+export default Navbar;
