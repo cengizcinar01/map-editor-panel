713a5b03f9ac6556de7ee14293803ea1cb6bdf73
Cengiz Cinar <c.cinar@mail.de>
2024-06-19 20:31:30 +0200

    Adding Logto Configuration and Environment Variables in Main.jsx and .gitignore Files

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,11 @@
+import { LogtoProvider } from "@logto/react";
 import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root")).render();
+const config = {
+  endpoint: `${import.meta.env.VITE_LOGTO_API_ENDPOINT}`,
+  appId: `${import.meta.env.VITE_LOGTO_APP_ID}`,
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <LogtoProvider config={config}></LogtoProvider>
+);
