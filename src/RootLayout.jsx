99f62473747fd745d89628f61739c3a0dba51a28
Cengiz Cinar <c.cinar@mail.de>
2024-06-19 22:27:19 +0200

    Updating ESLint Configuration and RootLayout Component

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,9 +1,22 @@
+import { useLogto } from "@logto/react";
 import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
+  const { signIn, isAuthenticated } = useLogto();
+
+  if (isAuthenticated) {
+    return (
+      <>
+        <Outlet />
+      </>
+    );
+  }
+
   return (
     <>
-      <Outlet />
+      <button onClick={() => signIn("http://localhost:3001/callback")}>
+        Sign In
+      </button>
     </>
   );
 };
