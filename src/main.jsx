491e2891f05ae1039fe2d4f8a05bcf2ee62f8138
Cengiz Cinar <c.cinar@mail.de>
2024-06-19 20:43:44 +0200

    Added routing and Home page to React application

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
 import { LogtoProvider } from "@logto/react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import RootLayout from "./RootLayout";
 
 const config = {
   endpoint: `${import.meta.env.VITE_LOGTO_API_ENDPOINT}`,
   appId: `${import.meta.env.VITE_LOGTO_APP_ID}`,
 };
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [{ index: true, element: <Home /> }],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <LogtoProvider config={config}></LogtoProvider>
+  <LogtoProvider config={config}>
+    <RouterProvider router={router} />
+  </LogtoProvider>
 );
