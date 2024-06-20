cc88dac10b9ca5879bdcf609d94b2ccf44f5e35c
Cengiz Cinar <c.cinar@mail.de>
2024-06-20 03:57:34 +0200

    Added logo to SignIn component and CSS styling

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -15,6 +15,11 @@ const SignIn = () => {
   return (
     <div className={styles["sign-in-button-container"]}>
       <div className={styles["wrapper"]}>
+        <img
+          className={styles["logo"]}
+          src="https://api.litehost.cc/assets/7acc0aec-f06d-4f5a-9800-53069b9de146"
+          alt="Map Editor Project Logo"
+        />
         <span>Access denied</span>
         <button
           className={styles["sign-in-button"]}
