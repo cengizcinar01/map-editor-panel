0077b067e267e4438278cefe386e2b387bc82666
Cengiz Cinar <c.cinar@mail.de>
2024-06-20 19:36:50 +0200

    Added authentication check for Sidebar component rendering

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,16 @@
+import { useLogto } from "@logto/react";
 import styles from "./styles/Sidebar.module.css";
 
 const Sidebar = () => {
-  return (
-    <>
-      <div className={styles["sidebar-container"]}></div>
-    </>
-  );
+  const { isAuthenticated } = useLogto();
+
+  if (isAuthenticated) {
+    return (
+      <>
+        <div className={styles["sidebar-container"]}></div>
+      </>
+    );
+  }
 };
 
 export default Sidebar;
