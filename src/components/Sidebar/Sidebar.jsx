56451c33eafc84e2d5fbd0924933dd4ab1eed50d
Cengiz Cinar <c.cinar@mail.de>
2024-06-21 00:32:06 +0200

    Removed authentication check from Sidebar Component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,18 @@
-import { useLogto } from "@logto/react";
 import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
-  const { isAuthenticated } = useLogto();
-
-  if (isAuthenticated) {
-    return (
-      <>
-        <ul>
-          <li>
-            <NavLink to="/">Dashboard</NavLink>
-          </li>
-          <li>
-            <NavLink to="/add-style">Add Style</NavLink>
-          </li>
-        </ul>
-      </>
-    );
-  }
+  return (
+    <>
+      <ul>
+        <li>
+          <NavLink to="/">Dashboard</NavLink>
+        </li>
+        <li>
+          <NavLink to="/add-style">Add Style</NavLink>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default Sidebar;
