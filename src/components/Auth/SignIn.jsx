5e9a6bc673688a7c2ed2f983e79f9d435f5c8606
Cengiz Cinar <c.cinar@mail.de>
2024-06-20 02:09:31 +0200

    Updated SignIn Component and Added Related CSS Styles

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -1,4 +1,5 @@
 import { useLogto } from "@logto/react";
+import styles from "./styles/SignIn.module.css";
 
 const SignIn = () => {
   const { signIn, isAuthenticated } = useLogto();
@@ -8,13 +9,19 @@ const SignIn = () => {
   }
 
   return (
-    <button
-      onClick={() =>
-        signIn(`${import.meta.env.VITE_LOGTO_REDIRECT_URL}/callback`)
-      }
-    >
-      Sign In
-    </button>
+    <div className={styles["sign-in-button-container"]}>
+      <div className={styles["wrapper"]}>
+        <span>Access denied</span>
+        <button
+          className={styles["sign-in-button"]}
+          onClick={() =>
+            signIn(`${import.meta.env.VITE_LOGTO_REDIRECT_URL}/callback`)
+          }
+        >
+          Sign In
+        </button>
+      </div>
+    </div>
   );
 };
 
