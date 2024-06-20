10c86ad4db4ae3f94cc51b39790583c97b91ee47
Cengiz Cinar <c.cinar@mail.de>
2024-06-20 19:35:00 +0200

    Adding Sidebar Component and Styles to Root Layout

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
 import SignIn from "./components/Auth/SignIn";
 import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import styles from "./styles/RootLayout.module.css";
 
 const RootLayout = () => {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <div className={styles["main-container"]}>
+        <Sidebar />
+        <Outlet />
+      </div>
       <SignIn />
     </>
   );
