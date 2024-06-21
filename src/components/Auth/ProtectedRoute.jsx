6df6e4b8544ff3cc01c990d923ee232172f9a62f
Cengiz Cinar <c.cinar@mail.de>
2024-06-21 11:01:05 +0200

    Simplified Return Statements in Callback and ProtectedRoute Components

--- src/components/Auth/ProtectedRoute.jsx
+++ src/components/Auth/ProtectedRoute.jsx
@@ -6,11 +6,7 @@ const ProtectedRoute = () => {
   const { isAuthenticated } = useLogto();
 
   if (isAuthenticated) {
-    return (
-      <>
-        <RootLayout />
-      </>
-    );
+    return <RootLayout />;
   }
 
   return <SignIn />;
