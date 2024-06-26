0d7eb34a6375676334ea684363a7a8ab10f08ba8
Cengiz Cinar <c.cinar@mail.de>
2024-06-26 22:16:03 +0200

    Adding Delete Functionality to Style List Component React App

--- src/pages/AddStyle/components/StyleList.jsx
+++ src/pages/AddStyle/components/StyleList.jsx
@@ -20,6 +20,19 @@ const StyleList = () => {
     fetchStyles();
   }, []);
 
+  const handleDeleteStyle = async (styleId) => {
+    try {
+      await axios.delete(
+        `${import.meta.env.VITE_APP_API_URL}/style/delete-style/${styleId}`
+      );
+      setGetAllStyles(
+        getAllStyles.filter((style) => style.style_id !== styleId)
+      );
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <>
       <div className={styles["style-list-container"]}>
@@ -34,6 +47,12 @@ const StyleList = () => {
                 className={styles["style-image"]}
               />
               <div className={styles["style-name"]}>{styleItem.style_name}</div>
+              <button
+                className={styles["delete-button"]}
+                onClick={() => handleDeleteStyle(styleItem.style_id)}
+              >
+                X
+              </button>
             </div>
           ))}
         </div>
