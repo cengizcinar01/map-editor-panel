da190219d91e2827825fafac03898bfd152d6288
Cengiz Cinar <c.cinar@mail.de>
2024-07-04 15:57:02 +0200

    Refactor AddFavoritePlace Component for Efficiency and Simplicity

--- src/pages/AddFavoritePlace/AddFavoritePlace.jsx
+++ src/pages/AddFavoritePlace/AddFavoritePlace.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const AddFavoritePlace = () => {
+  const { getAccessToken, isAuthenticated } = useLogto();
   const [favoritePlaces, setFavoritePlaces] = useState([]);
   const [newPlace, setNewPlace] = useState({
     name: "",
@@ -12,86 +13,81 @@ const AddFavoritePlace = () => {
     longitude: "",
   });
 
-  const { getAccessToken, isAuthenticated } = useLogto();
-
   useEffect(() => {
-    const fetchFavoritePlaces = async () => {
-      if (isAuthenticated) {
-        const accessToken = await getAccessToken(
-          `${import.meta.env.VITE_LOGTO_RESOURCES}`
-        );
-        try {
-          const response = await axios.get(
-            "http://localhost:5000/favorite/get-favorite-place",
-            {
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-              },
-            }
-          );
-          setFavoritePlaces(response.data);
-        } catch (error) {
-          console.error("Error fetching favorite places:", error);
-        }
-      }
-    };
+    if (isAuthenticated) {
+      fetchFavoritePlaces();
+    }
+  }, [isAuthenticated]);
 
-    fetchFavoritePlaces();
-  }, [isAuthenticated, getAccessToken]);
+  const fetchFavoritePlaces = async () => {
+    const accessToken = await getAccessToken(
+      `${import.meta.env.VITE_LOGTO_RESOURCES}`
+    );
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_APP_API_URL}/favorite/get-favorite-place`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      setFavoritePlaces(response.data);
+    } catch (error) {
+      console.error("Error fetching favorite places:", error);
+    }
+  };
 
   const handleInputChange = (event) => {
     setNewPlace({ ...newPlace, [event.target.name]: event.target.value });
   };
 
   const handleAddPlace = async () => {
-    if (isAuthenticated) {
-      const accessToken = await getAccessToken(
-        `${import.meta.env.VITE_LOGTO_RESOURCES}`
+    const accessToken = await getAccessToken(
+      `${import.meta.env.VITE_LOGTO_RESOURCES}`
+    );
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_APP_API_URL}/favorite/add-favorite-place`,
+        newPlace,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
       );
-      try {
-        const response = await axios.post(
-          "http://localhost:5000/favorite/add-favorite-place",
-          newPlace,
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          }
-        );
-        setFavoritePlaces([...favoritePlaces, response.data]);
-        setNewPlace({
-          name: "",
-          country: "",
-          flag: "",
-          latitude: "",
-          longitude: "",
-        });
-      } catch (error) {
-        console.error("Error adding favorite place:", error);
-      }
+      setFavoritePlaces([...favoritePlaces, response.data]);
+      setNewPlace({
+        name: "",
+        country: "",
+        flag: "",
+        latitude: "",
+        longitude: "",
+      });
+      await fetchFavoritePlaces();
+    } catch (error) {
+      console.error("Error adding favorite place:", error);
     }
   };
 
   const handleRemovePlace = async (placeId) => {
-    if (isAuthenticated) {
-      const accessToken = await getAccessToken(
-        `${import.meta.env.VITE_LOGTO_RESOURCES}`
+    const accessToken = await getAccessToken(
+      `${import.meta.env.VITE_LOGTO_RESOURCES}`
+    );
+    try {
+      await axios.delete(
+        `${
+          import.meta.env.VITE_APP_API_URL
+        }/favorite/delete-favorite-place/${placeId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
       );
-      try {
-        await axios.delete(
-          `http://localhost:5000/favorite/delete-favorite-place/${placeId}`,
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          }
-        );
-        setFavoritePlaces(
-          favoritePlaces.filter((place) => place.id !== placeId)
-        );
-      } catch (error) {
-        console.error("Error removing favorite place:", error);
-      }
+      setFavoritePlaces(favoritePlaces.filter((place) => place.id !== placeId));
+    } catch (error) {
+      console.error("Error removing favorite place:", error);
     }
   };
 
@@ -103,41 +99,16 @@ const AddFavoritePlace = () => {
     <div>
       <h2>Add Favorite Place</h2>
       <div>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={newPlace.name}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="country"
-          placeholder="Country"
-          value={newPlace.country}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="flag"
-          placeholder="Flag"
-          value={newPlace.flag}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="latitude"
-          placeholder="Latitude"
-          value={newPlace.latitude}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="longitude"
-          placeholder="Longitude"
-          value={newPlace.longitude}
-          onChange={handleInputChange}
-        />
+        {Object.entries(newPlace).map(([key, value]) => (
+          <input
+            key={key}
+            type="text"
+            name={key}
+            placeholder={key.charAt(0).toUpperCase() + key.slice(1)}
+            value={value}
+            onChange={handleInputChange}
+          />
+        ))}
         <button onClick={handleAddPlace}>Add Place</button>
       </div>
       <h2>Favorite Places</h2>
