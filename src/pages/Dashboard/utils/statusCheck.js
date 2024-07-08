2f725619cd01cb87f588c52f21ef6faa8c9752f4
Cengiz Cinar <c.cinar@mail.de>
2024-07-08 23:30:10 +0200

    Update to use environment variable for API URL in statusCheck.js

--- src/pages/Dashboard/utils/statusCheck.js
+++ src/pages/Dashboard/utils/statusCheck.js
@@ -2,9 +2,9 @@ export const checkStatus = async (resources) => {
   const status = await Promise.all(
     resources.map(async ({ url }) => {
       try {
-        const proxyUrl = `http://localhost:5000/proxy?url=${encodeURIComponent(
-          url
-        )}`;
+        const proxyUrl = `${
+          import.meta.env.VITE_APP_API_URL
+        }/proxy?url=${encodeURIComponent(url)}`;
         const response = await fetch(proxyUrl);
         return response.ok ? "online" : "offline";
       } catch (error) {
@@ -12,6 +12,5 @@ export const checkStatus = async (resources) => {
       }
     })
   );
-
   return Object.fromEntries(resources.map(({ name }, i) => [name, status[i]]));
 };
