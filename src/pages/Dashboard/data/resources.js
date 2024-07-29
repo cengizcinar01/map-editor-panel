4e72450157c91ab233be14412be8f40c63829a99
Cengiz Cinar <c.cinar@mail.de>
2024-07-29 02:03:16 +0200

    Updated URLs for 'map-editor-render' and 'map-editor-frame' in resources.js

--- src/pages/Dashboard/data/resources.js
+++ src/pages/Dashboard/data/resources.js
@@ -2,7 +2,13 @@ export const resources = [
   { name: "map-editor-api", url: "https://api.createyourmap.com/health" },
   { name: "map-editor-panel", url: "https://panel.createyourmap.com" },
   { name: "map-editor-web", url: "https://createyourmap.com" },
-  { name: "map-editor-render", url: "https://render.createyourmap.com/health" },
-  { name: "map-editor-frame", url: "https://frame.createyourmap.com/health" },
+  {
+    name: "map-editor-render",
+    url: "https://c4sc0wk.116.202.11.36.sslip.io/health",
+  },
+  {
+    name: "map-editor-frame",
+    url: "https://d4kkgsc.116.202.11.36.sslip.io/health",
+  },
   { name: "logto", url: "https://logto.createyourmap.com" },
 ];
