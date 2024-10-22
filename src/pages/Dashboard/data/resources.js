9e70ba6697214cdd497d4cd9780e9cafa5942e74
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-10-23 01:31:52 +0200

    Update resources.js

--- src/pages/Dashboard/data/resources.js
+++ src/pages/Dashboard/data/resources.js
@@ -4,11 +4,11 @@ export const resources = [
   { name: "map-editor-web", url: "https://createyourmap.com" },
   {
     name: "map-editor-render",
-    url: "https://mkscgsw4swkkgog0o08kwws8.188.245.90.1.sslip.io/health",
+    url: "https://createyourmap.com",
   },
   {
     name: "map-editor-frame",
-    url: "https://gwsss8g4wos4cks4w8cs8c4s.188.245.90.1.sslip.io/health",
+    url: "https://createyourmap.com",
   },
   { name: "logto", url: "https://logto.createyourmap.com" },
 ];
